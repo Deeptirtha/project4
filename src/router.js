53c235c9edb9c55af726eb45ddaca0799bc0674f
Deeptirtha <tirtha1111222@gmail.com>
2022-12-14 18:33:37 +0530

    commit

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
-
 const express=require("express")
 const router=express.Router()
 const {creatUrl,geturl}= require("./controller/controller")
 
+//========================================================Create short url=======================================================================
 router.post("/url/shorten",creatUrl)
-
+//========================================================Get short url==========================================================================
 router.get("/:urlCode",geturl)
-
+//=======================================================Wrong api===============================================================================
 router.all("/*", function (req, res) {
     res.status(404).send({status: false,msg: "Wrong api please try different"})})
 
